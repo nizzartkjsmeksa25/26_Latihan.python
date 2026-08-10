6e03c4242619a0675667d70e7fafd6f1e29037b0
26_NizzarShifanA <nizzar.tkjsmeksa25@gmail.com>
2026-08-10 11:18:51 +0700

    Add files via upload

---
+++ genapganjil.py
@@ -0,0 +1,8 @@
+for i in range(5):
+    bilangan = int(input("Masukkan bilangan: "))
+
+    if bilangan % 2 == 0:
+        print(bilangan, "adalah bilangan genap")
+    else:
+        print(bilangan, "adalah bilangan ganjil")
+ 
