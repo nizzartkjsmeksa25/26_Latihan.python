2611aaccaf493b6bbf7fadbb53d5a64a5f6d4ef6
26_NizzarShifanA <nizzar.tkjsmeksa25@gmail.com>
2026-08-31 10:20:03 +0700

    Add files via upload

---
+++ modul.py
@@ -0,0 +1,15 @@
+import fungsi
+print("Program ini akan menampilkan bilangan ganjil dan genap ")
+fungsi.ganjil_genap()
+
+import fungsi
+print("Program ini akan melakukan perkalian ")
+fungsi.perkalian()
+
+import fungsi
+print("Program ini akan menghitung keliling persegi ")
+fungsi.kelilingpersegi()
+
+import fungsi
+print("Program ini akan menghitung luas persegi ")
+fungsi.luaspersegi()
